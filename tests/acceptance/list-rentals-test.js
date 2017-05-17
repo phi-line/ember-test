77f4614f7b7f5ea797b34f473780d748de3faa7b
phi- <phi.liney@gmail.com>
2017-05-17 11:00:37 -0700

    fixed bug with integration tests

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -33,23 +33,6 @@ test('should list available rentals.', function (assert) {
   });
 });
 
-test('should display rental details', function(assert) {
-  this.set('rentalObj', rental);
-  this.render(hbs`{{rental-listing rental=rentalObj}}`);
-  assert.equal(this.$('.listing h3').text(), 'test-title', 'Title: test-title');
-  assert.equal(this.$('.listing .owner').text().trim(), 'Owner: test-owner');
-});
-
-test('should toggle wide class on click', function(assert) {
-  this.set('rentalObj', rental);
-  this.render(hbs`{{rental-listing rental=rentalObj}}`);
-  assert.equal(this.$('.image.wide').length, 0, 'initially rendered small');
-  this.$('.image').click();
-  assert.equal(this.$('.image.wide').length, 1, 'rendered wide after click');
-  this.$('.image').click();
-  assert.equal(this.$('.image.wide').length, 0, 'rendered small after second click');
-});
-
 test('should filter the list of rentals by city.', function (assert) {
 });
 
